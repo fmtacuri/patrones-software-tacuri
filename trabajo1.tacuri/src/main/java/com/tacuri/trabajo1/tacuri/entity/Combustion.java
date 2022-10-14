972b2c2f27ceddcb74f32242f5db8911b2b32fa3
Freddy Tacuri <ftacurip@pichincha.com>
2022-10-13 22:08:28 -0500

    [feature] add herencia

---
+++ trabajo1.tacuri/src/main/java/com/tacuri/trabajo1/tacuri/entity/Combustion.java
@@ -0,0 +1,17 @@
+package com.tacuri.trabajo1.tacuri.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Combustion extends Marca {
+  String tipoGasolina;
+}
