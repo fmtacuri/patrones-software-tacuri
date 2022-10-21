eb4e26e1c5fd4134f923805abf1d1dc3c75790c8
Freddy Tacuri <ftacurip@pichincha.com>
2022-10-20 21:20:43 -0500

    [feature] add singleton without codesmell

--- trabajo1.tacuri/src/main/java/com/tacuri/singleton/ObjectMapperSingleton.java
+++ trabajo1.tacuri/src/main/java/com/tacuri/singleton/ObjectMapperSingleton.java
@@ -2,10 +2,13 @@ package com.tacuri.singleton;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public final class ObjectMapperSingleton {
+public class ObjectMapperSingleton {
 
   private static ObjectMapper objectMapper;
 
+  private ObjectMapperSingleton() {
+    throw new IllegalStateException("Utility class");
+  }
   public static ObjectMapper getInstance() {
     if (objectMapper == null) {
       objectMapper = new ObjectMapper();
